d3c5bb4761aaf2f27b51df980b46cdaa075d7559
nishanthan <nishanthan@codevus.com>
2022-12-03 20:30:38 +0530

    projects card

---
+++ src/components/Projects/data.js
@@ -0,0 +1,33 @@
+import LogoS from '../../assets/images/n-logo-c.png'
+export const dataProjects = [
+  {
+    id: 1,
+    title: 'Project 01',
+    techs: 'HTML, CSS, JavaScript, PHP',
+    link: 'LogoS',
+  },
+  {
+    id: 1,
+    title: 'Project 01',
+    techs: 'HTML, CSS, JavaScript, PHP',
+    link: '../../assets/images/n-logo-c.png',
+  },
+  {
+    id: 1,
+    title: 'Project 01',
+    techs: 'HTML, CSS, JavaScript, PHP',
+    link: '/../../../assets/images/n-logo-c.png',
+  },
+  {
+    id: 1,
+    title: 'Project 01',
+    techs: 'HTML, CSS, JavaScript, PHP',
+    link: '/../../assets/images/n-logo-c.png',
+  },
+  {
+    id: 1,
+    title: 'Project 01',
+    techs: 'HTML, CSS, JavaScript, PHP',
+    link: '../../assets/images/n-logo-c.png',
+  },
+]
---
+++ src/components/Projects/index.js
@@ -0,0 +1,84 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import React, { Component } from 'react'
+import Loader from 'react-loaders'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import { dataProjects } from './data'
+
+const Projects = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              // letterClass={letterClass}
+              strArray={['M', 'y', ' ', 'P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially on ambitious
+            or large projects. However, if you have any other requests or
+            questions, don't hesitate to contact me using below form either.
+          </p>
+        </div>
+      </div>
+      <div className="card-container">
+        <Slider {...settings}>
+          {dataProjects.map((item) => (
+            <div className="card">
+              <div className="card-top">
+                <img src={item.link} alt={item.title} />
+                <h1>{item.title}</h1>
+              </div>
+              <div className="card-bottom">
+                <h3>{item.techs}</h3>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Projects
