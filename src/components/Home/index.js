b22d6bc02c902e52ab60fae142aed51abeaa2626
nishanthan <nishanthan@codevus.com>
2022-12-06 18:34:03 +0530

    color & font changes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,21 +3,43 @@ import { Link } from 'react-router-dom'
 import './index.scss'
 import Logo from './Logo'
 import Loader from 'react-loaders'
+import Typewriter from 'typewriter-effect'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [state] = useState({
+    title: 'Hi,',
+    titleTwo: "I'm ",
+    titleThree: 'Nishanthan.',
+  })
   return (
     <>
       <div className="container home-page">
         <div className="text-zone">
           <h1>
-            Hi, <br />
-            I'm
-            <img src={LogoTitle} alt="developer" />
-            ishanthan
-            <br />
-            web developer
+            <div className="firstTitle">
+              {state.title} <br />
+            </div>
+            <div className="secondTitle">
+              {state.titleTwo}
+              {state.titleThree}
+            </div>
           </h1>
-          <h2>Frontend Developer / Software Engineer / Mobile App Developer</h2>
+          <div className="myServices">
+            <Typewriter
+              options={{
+                autoStart: true,
+                loop: true,
+                delay: 40,
+                strings: [
+                  "I'm a Software Engineer.",
+                  "I'm a Frontend Engineer.",
+                  "I'm a Web Developer.",
+                  "I'm an Android Developer.",
+                ],
+              }}
+            />
+          </div>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
